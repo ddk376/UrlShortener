b9e4885fda925c10e272cf91b6204e0509284e01
Gina&Chloe <gina&chloe@appacademy.io>
2015-08-12 15:26:39 -0400

    Create User database and User model.

--- db/migrate/20150812190221_create_users.rb
+++ db/migrate/20150812190221_create_users.rb
@@ -1,10 +1,11 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :email
+      t.string :email, unique: true
 
       t.timestamps
     end
+
+    add_index :users, :email
   end
-  add_index :users :email
 end
